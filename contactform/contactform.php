406788bbd231aa2e32d0ad5200b274d87fd835e7
CACTOSMEDIA <cactosmedia@gmail.com>
2023-03-25 02:30:49 -0400

    cactosmedia

---
+++ contactform/contactform.php
@@ -0,0 +1,31 @@
+<?php
+// Get the form data
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+// Connect to the database
+$servername = "localhost";
+$username = "yourusername";
+$password = "yourpassword";
+$dbname = "yourdatabase";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+// Insert the form data into the database
+$sql = "INSERT INTO contacts (name, email, message)
+VALUES ('$name', '$email', '$message')";
+
+if (mysqli_query($conn, $sql)) {
+  echo "Message sent successfully!";
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+// Close the database connection
+mysqli_close($conn);
+?>
